113ccb2c650796f2a1f6e96ea321949c4975c8bb
Nic Cheneweth <nchenewe@thoughtworks.com>
2023-03-07 11:03:47 -0600

    [nc] pipeline setup
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import setuptools
+import versioneer
 
 with open("README.md", "r", encoding = "utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name = "iam-credential-rotation",
-    version = "0.0.1",
+    version=versioneer.get_version(),
     author = "Nic Cheneweth",
     author_email = "nchenewe@thoughtworks.com",
     description = "automated rotation for iam machine account programmatic credentials",
@@ -19,6 +20,7 @@ setuptools.setup(
     package_dir = {"": "src"},
     packages = setuptools.find_packages(where="src"),
     python_requires = ">=3.10",
+    cmdclass=versioneer.get_cmdclass(),
     entry_points = '''
         [console_scripts]
         iam-credential-rotation=iam_credential_rotation:cli
